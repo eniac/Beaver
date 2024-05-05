0c6886f57a5eadabfe292d84b20d6e2afda4f278
liangcheng-yu <liangcheng.yu46@gmail.com>
2024-05-04 21:25:34 -0400

    chore: Enforce consistent commit msg

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ echo "Setting up the repository..."
 
 HOOKS_DIR=${PWD}/.git/hooks
 cp scripts/pre-commit $HOOKS_DIR/pre-commit
+cp scripts/commit-msg $HOOKS_DIR/commit-msg
 
 pip3 install -r requirements.txt
 
